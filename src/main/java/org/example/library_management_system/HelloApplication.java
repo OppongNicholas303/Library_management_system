1e5015dda3167a912063b5f4c8ecc0696302425e
nicholas <nicholas.oppong@amalitech.com>
2024-11-15 14:29:20 +0000

    main

--- src/main/java/org/example/library_management_system/HelloApplication.java
+++ src/main/java/org/example/library_management_system/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
 
         stage.setMaximized(true);  // Set the window to open maximized
-        stage.show();gi
+        stage.show();
     }
 
     public static void main(String[] args) {
